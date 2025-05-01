86f43d0817201744a3e116c31cc7dd6a77877c8e
tiennguyenTCU <nguyentien3036@gmail.com>
2025-04-30 19:19:39 -0500

    style: fix unexpected ui bug in mobile screens

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,29 +1,28 @@
 <template>
   <div
     class="fixed inset-x-0 top-0 z-50 duration-200 group bg-gradient-to-b from-white lg:from-white/75 opacity-100 -translate-y-0">
-    <div class="mx-auto w-full px-4 sm:px-6 lg:px-8 xl:max-w-7xl relative">
-      <nav class="flex items-center justify-between gap-4 duration-200 py-4">
-        <div class="flex gap-4 items-center">
-          <UButton color="white" variant="ghost" size="xl" icon="i-heroicons:bars-3-solid" class="lg:hidden" square
+    <div class="mx-auto w-full px-2 sm:px-6 lg:px-8 xl:max-w-7xl relative">
+      <nav class="flex items-center justify-between gap-2 duration-200 py-2 lg:py-4">
+        <div class="flex gap-2 items-center">
+          <UButton color="white" variant="ghost" size="lg" icon="i-heroicons:bars-3-solid" class="lg:hidden" square
             padded @click="isOpenMenu = true" />
           <USlideover v-model="isOpenMenu" side="left">
-            <div class="p-8 mb-4">
+            <div class="p-4 mb-2">
               <UButton color="gray" variant="ghost" size="sm" icon="i-heroicons-x-mark-20-solid"
-                class="flex absolute end-5 top-5 z-10" square padded @click="isOpenMenu = false" />
-              <div class="flex flex-1 flex-col gap-2">
-                <NuxtLink class="text-slate-500 text-xl px-3 py-4 hover:text-slate-900"
+                class="flex absolute end-2 top-2 z-10" square padded @click="isOpenMenu = false" />
+              <div class="flex flex-1 flex-col gap-1">
+                <NuxtLink class="text-slate-500 text-base px-2 py-2 hover:text-slate-900"
                   :class="{ 'text-slate-900 underline': route.path === '/' }" to="/" @click="isOpenMenu = false">Home
                 </NuxtLink>
                 <NuxtLink v-for="item in navItems" :key="item.path"
-                  class="text-slate-500 text-xl px-3 py-4 hover:text-slate-900"
+                  class="text-slate-500 text-base px-2 py-2 hover:text-slate-900"
                   :class="{ 'text-slate-900 underline': route.path === item.path }" :to="item.path"
                   @click="isOpenMenu = false">{{ item.name }}</NuxtLink>
               </div>
             </div>
           </USlideover>
           <NuxtLink to="/">
-            <!-- <NuxtImg preload :class="route.path === '/' ? 'w-8 h-10' : 'w-9 h-8'" :src="route.path === '/' ? '/logo_image.png' : '/logo_image_black.png'" /> -->
-            <NuxtImg preload class="'w-10 h-10" src="/logo_image_black.png" />
+            <NuxtImg preload class="w-8 h-8 lg:w-10 lg:h-10" src="/logo_image_black.png" />
           </NuxtLink>
         </div>
 
@@ -33,11 +32,10 @@
             :class="{ 'text-slate-900 underline': route.path === item.path }" :to="item.path"
             @click="isOpenMenu = false">{{ item.name }}</NuxtLink>
         </div>
-        <div class="flex items-center gap-3">
-          <button v-for="locale in locales" :key="locale.code" @click="clicked(locale.code)"
-            class="flex items-center gap-2">
-            <img :src="`/${locale.code === 'vi' ? 'vietnamese-flag' : 'english-flag'}.png`" class="w-6 h-4"
-              :alt="locale.name" />
+        <div class="flex items-center gap-2">
+          <button v-for="locale in locales" :key="locale.code" @click="clicked(locale.code)" class="flex items-center">
+            <img :src="`/${locale.code === 'vi' ? 'vietnamese-flag' : 'english-flag'}.png`"
+              class="w-5 h-3 lg:w-6 lg:h-4" :alt="locale.name" />
           </button>
         </div>
       </nav>
--- components/home/banner/index.vue
+++ components/home/banner/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="relative w-full aspect-[16/9] sm:aspect-[16/9] overflow-hidden border-b border-slate-200 pt-12">
-    <div class="flex w-full h-full pointer-events-none">
-      <InspiraSpline :scene="sceneUrl" class="w-full h-full mt-12" />
+  <div class="relative w-full aspect-[12/9] sm:aspect-[16/9] overflow-hidden border-b border-slate-200 no-padding-md-range ">
+    <div class="flex sm:w-full h-full pointer-events-none">
+      <InspiraSpline :scene="sceneUrl" class="sm:w-full sm:h-full sm:mt-12" />
     </div>
   </div>
 </template>
@@ -9,3 +9,12 @@
 <script setup lang="ts">
 const sceneUrl = "https://prod.spline.design/HMi1Hcw6Dd1j7Uio/scene.splinecode";
 </script>
+
+<style scoped>
+@media (max-width: 768px), (min-width: 992px) {
+  .no-padding-md-range {
+    padding-top: 3rem;
+  }
+}
+</style>
+
